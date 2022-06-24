a1c680085e0f23031dc1330d0fef4af8dd70e401
wottreng <89848850+wottreng@users.noreply.github.com>
2022-06-23 20:09:32 -0400

    v3.0 multi file algo v2

--- src/data_utils/data_utils.go
+++ src/data_utils/data_utils.go
@@ -25,20 +25,16 @@ func Get_JSON_data(topic string, rows int) string {
 		filename := system_utils.Get_latest_file_in_directory(directory_path, topic)
 		data_string := file_utils.Read_string_from_file(directory_path, filename)
 		data_split := strings.Split(data_string, "\n")
-		data_split = reverse_list(data_split[0 : len(data_split)-1])
+		data_split = data_split[0 : len(data_split)-1]
 		return "{\"data\":[" + data_split[0] + "]}"
 	}
-	// if rows > 0 then return last rows in latest file
+	// if rows > 0 then return `rows` of data
 	if rows > 0 {
 		filename := system_utils.Get_latest_file_in_directory(directory_path, topic)
 		data_string := file_utils.Read_string_from_file(directory_path, filename)
 		data_split := strings.Split(data_string, "\n")
-		data_split = reverse_list(data_split[0 : len(data_split)-1])
-		//println("[-->] file: ", filename)
-		//println("[DEBUG] len(data_split): ", len(data_split))
-		//println("[DEBUG] rows: ", rows)
+		data_split = data_split[0 : len(data_split)-1]
 		if len(data_split) >= rows { // if more data than rows then return rows
-			//println("[DEBUG] first file only")
 			data_split = data_split[0:rows]
 			all_data := strings.Join(data_split, ",")
 			json_data := "{\"data\":[" + all_data + "]}"
@@ -46,27 +42,19 @@ func Get_JSON_data(topic string, rows int) string {
 		}
 		// if the number of lines in the file is less than the number of rows requested
 		additional_rows := rows - len(data_split)
-		//println("[DEBUG] additional_rows: ", additional_rows)
 		// get aditional rows from previous files
 		all_files := system_utils.List_files_in_directory(directory_path, topic)
 		for i := 1; i < len(all_files); i++ {
-			//print_list(data_split)
-			//println("[-->] file: ", all_files[i])
 			data_string_local := file_utils.Read_string_from_file(directory_path, all_files[i])
 			data_split_local := strings.Split(data_string_local, "\n")
-			data_split_local = reverse_list(data_split_local[0 : len(data_split_local)-1])
-			//println("[DEBUG] len(data_split_local): ", len(data_split_local))
+			data_split_local = data_split_local[0 : len(data_split_local)-1]
 			if (len(data_split_local)) < additional_rows {
-				//println("[DEBUG] add data")
 				additional_rows -= (len(data_split_local))
-				//println("[DEBUG] additional_rows: ", additional_rows)
-				data_split = append(data_split, data_split_local[0:len(data_split_local)]...)
-				//println("[DEBUG] len(data_split): ", len(data_split))
+				data_split_local = data_split_local[0:len(data_split_local)]
+				data_split = append(data_split_local, data_split...)
 			} else {
-				//println("[DEBUG] break")
-				//println("[DEBUG] additional_rows: ", additional_rows)
-				//println("[DEBUG] len(data_split_local): ", len(data_split_local))
-				data_split = append(data_split, data_split_local[0:additional_rows]...)
+				data_split_local = data_split_local[0:additional_rows]
+				data_split = append(data_split_local, data_split...)
 				break
 			}
 		}
